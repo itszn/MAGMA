1308b2b2f289708f686ee8c7bea1cca5955bf1a1
- <->
2016-11-28 21:01:45 -0500

    init FmtPass

--- Magma.cpp
+++ Magma.cpp
@@ -9,12 +9,46 @@
 using namespace llvm;
 
 namespace {
+    struct FmtPass : public InstVisitor<FmtPass>
+    {
+        FmtPass() {}
+
+        virtual void visitCallInst(CallInst &I)
+        {
+            if (I.getCalledFunction()->getName() == "printf")
+            {
+                Value * op0 = I.getOperand(0)->stripPointerCasts();
+                errs() << *op0 << "\n";
+
+                if (isa<GlobalVariable>(op0) && isa<Constant>(op0))
+                {
+                    GlobalVariable * g = dyn_cast<GlobalVariable>(op0);
+                    StringRef val = dyn_cast<ConstantDataSequential>(g->getInitializer())->getAsCString();
+                    if (val == "%s" || val == "%s\n")
+                    {
+                        errs() << "blah\n";
+                    }
+                }
+            }
+
+        }
+
+        virtual void visitInvokeInst(InvokeInst &I)
+        {
+            errs() << "invoke: " << I << "\n";
+        }
+    };
+
     struct Magma: public FunctionPass {
         static char ID;
         Magma() : FunctionPass(ID) {}
 
 
         bool runOnFunction(Function &F) override {
+
+            FmtPass fmt;
+            fmt.visit(F);
+
             return true;
         }
     };
