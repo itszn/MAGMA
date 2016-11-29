50589d10fc9cf232dd51acdcd485f9e0a7d3cc45
itszn <itszn@itszn.com>
2016-11-28 23:02:07 -0500

    General cleanup

--- Magma.cpp
+++ Magma.cpp
@@ -9,20 +9,18 @@
 using namespace llvm;
 
 namespace {
-    struct FmtPass : public InstVisitor<FmtPass>
-    {
+
+
+    struct FmtPass : public InstVisitor<FmtPass> {
         FmtPass() {}
 
-        virtual void visitCallInst(CallInst &I)
-        {
-            if (I.getCalledFunction() && I.getCalledFunction()->getName() == "printf")
-            {
+        virtual void visitCallInst(CallInst &I) {
+            if (I.getCalledFunction() && I.getCalledFunction()->getName() == "printf") {
                 Value * op0 = I.getArgOperand(0)->stripPointerCasts();
                 errs() << *op0 << "\n";
 
 
-                if (isa<GlobalVariable>(op0) && isa<Constant>(op0))
-                {
+                if (isa<GlobalVariable>(op0) && isa<Constant>(op0)) {
                     GlobalVariable * g = dyn_cast<GlobalVariable>(op0);
                     StringRef val = dyn_cast<ConstantDataSequential>(g->getInitializer())->getAsCString();
 
@@ -47,27 +45,18 @@ namespace {
                         std::vector<Value*> args0;
                         args0.push_back(builder.CreateGlobalStringPtr(s0));
 
-                        int argOffset = 0;
                         if (s0.size()>0) {
                             for (int i=0; i<s0.size()-1; i++) {
-                                if (s0[i]=='%' && s0[i+1]!='%') {
+                                if (s0[i]=='%' && s0[i+1]!='%')
                                     args0.push_back(*(opiter++));
-                                    argOffset++;
-                                }
                             }
-                            errs() << "Part 1 has " << argOffset << " args\n";
                             ArrayRef<Value*> args0_a(args0);
-                            errs() << "Created " <<  *CallInst::Create(I.getCalledFunction(), args0_a, "", &I) << "\n";
+                            CallInst::Create(I.getCalledFunction(), args0_a, "", &I);
                         }
                         std::vector<Value*> args1;
                         args1.push_back(*(opiter++));
                         ArrayRef<Value*> args1_a(args1);
-                        errs() << "Created " << *CallInst::Create(I.getCalledFunction(), args1_a, "", &I) << "\n";
-
-                            
-
-
-                        
+                        CallInst::Create(I.getCalledFunction(), args1_a, "", &I);
                     }
                     // Place the last printf if it needed to be split
                     if (found && val.size()>0) {
@@ -76,18 +65,13 @@ namespace {
                         for(; opiter!=I.op_end(); opiter++)
                             args.push_back(*opiter);
                         ArrayRef<Value*> args_a(args);
-                        errs() << "Created " << *CallInst::Create(I.getCalledFunction(), args_a, "", &I) << "\n";
-
+                        CallInst::Create(I.getCalledFunction(), args_a, "", &I);
+                        // Remove the real printf
                         I.eraseFromParent();
                     }
                 }
             }
 
-        }
-
-        virtual void visitInvokeInst(InvokeInst &I)
-        {
-            errs() << "invoke: " << I << "\n";
         }
     };
 
